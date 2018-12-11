4dd9e8edc0aacf5f818600d97fcb120f751b1e1b
lyttonlee <lzr3278@163.com>
2018-12-11 13:31:59 +0800

    vue-resume 2018-12

---
+++ README.md
@@ -0,0 +1,5 @@
+## my resume
+
+### resource code
+
+[github](https://github.com/lyttonlee/vue-resume)
---
+++ precache-manifest.ec8590d785864a54594ac37bd17343fc.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f69464f2177e3db869a",
+    "url": "/css/chunk-b6c62146.5688f180.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "8f69464f2177e3db869a",
+    "url": "/js/chunk-b6c62146.464d18e9.js"
+  },
+  {
+    "revision": "85377fc22f0de7b90e56",
+    "url": "/css/chunk-197b1126.71d0bd5a.css"
+  },
+  {
+    "revision": "97a9c0f131787509ecd6",
+    "url": "/css/chunk-22feaf26.c8926019.css"
+  },
+  {
+    "revision": "97a9c0f131787509ecd6",
+    "url": "/js/chunk-22feaf26.2c4299a8.js"
+  },
+  {
+    "revision": "05f5e6053aecd8039d64",
+    "url": "/css/chunk-3d9359b5.723930f7.css"
+  },
+  {
+    "revision": "05f5e6053aecd8039d64",
+    "url": "/js/chunk-3d9359b5.ec5f8286.js"
+  },
+  {
+    "revision": "938738d347bc200075be",
+    "url": "/js/app.eae5ae05.js"
+  },
+  {
+    "revision": "85377fc22f0de7b90e56",
+    "url": "/js/chunk-197b1126.a7d5787b.js"
+  },
+  {
+    "revision": "e220bed674b7286b55ab",
+    "url": "/css/chunk-f83df5cc.2c5b190e.css"
+  },
+  {
+    "revision": "e220bed674b7286b55ab",
+    "url": "/js/chunk-f83df5cc.6310b526.js"
+  },
+  {
+    "revision": "353ee26441d66fa2f3d8",
+    "url": "/css/chunk-vendors.8d15cdd8.css"
+  },
+  {
+    "revision": "353ee26441d66fa2f3d8",
+    "url": "/js/chunk-vendors.8a286746.js"
+  },
+  {
+    "revision": "e246fb2c1903d4d419ee6c739bc008c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "938738d347bc200075be",
+    "url": "/css/app.7b103bf7.css"
+  }
+];
